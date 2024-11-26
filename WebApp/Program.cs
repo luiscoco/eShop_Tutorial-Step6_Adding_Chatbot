dd73f51eb06af038a0ab6b31eb544c125340be2a
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-11-26 12:49:36 +0100

    Add project files.

---
+++ WebApp/Program.cs
@@ -0,0 +1,50 @@
+
+using WebApp.Components;
+using eShop.WebApp.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+.AddInteractiveServerComponents();
+
+builder.AddApplicationServices();
+
+//builder.AddAuthenticationServices();
+
+//builder.Services.AddHttpForwarderWithServiceDiscovery();
+
+//builder.Services.AddSingleton<IProductImageUrlProvider, ProductImageUrlProvider>();
+//builder.Services.AddHttpClient<CatalogService>(o => o.BaseAddress = new("http://localhost:5301"))
+//    .AddApiVersion(1.0);
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapForwarder("/product-images/{id}", "http://localhost:5301", "/api/catalog/items/{id}/pic");
+
+app.Run();
+
---
+++ eShop.AppHost/Program.cs
@@ -0,0 +1,40 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("postgres")
+    .WithImage("ankane/pgvector")
+    .WithImageTag("latest")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var catalogDb = postgres.AddDatabase("catalogdb");
+var identityDb = postgres.AddDatabase("IdentityDB");
+
+var launchProfileName = ShouldUseHttpForEndpoints() ? "http" : "https";
+
+var identityApi = builder.AddProject<Projects.Idintity_API>("identity-api", launchProfileName)
+    .WithExternalHttpEndpoints()
+    .WithReference(identityDb);
+
+var identityEndpoint = identityApi.GetEndpoint(launchProfileName);
+
+var catalogApi = builder.AddProject<Projects.Catalog_API>("catalog-api")
+    .WithReference(catalogDb);
+
+var webApp = builder.AddProject<Projects.WebApp>("webapp", launchProfileName)
+    .WithExternalHttpEndpoints()
+    .WithReference(catalogApi)
+    .WithEnvironment("IdentityUrl", identityEndpoint);
+
+webApp.WithEnvironment("CallBackUrl", webApp.GetEndpoint(launchProfileName));
+
+// Identity has a reference to all of the apps for callback urls, this is a cyclic reference
+identityApi.WithEnvironment("WebAppClient", webApp.GetEndpoint(launchProfileName));
+
+builder.Build().Run();
+static bool ShouldUseHttpForEndpoints()
+{
+    const string EnvVarName = "ESHOP_USE_HTTP_ENDPOINTS";
+    var envValue = Environment.GetEnvironmentVariable(EnvVarName);
+
+    // Attempt to parse the environment variable value; return true if it's exactly "1".
+    return int.TryParse(envValue, out int result) && result == 1;
+}
